7de3f1b466b37d9a5b9fc020f64433925d4a5447
Emil John Llanes <emiljohnllanes772@gmail.com>
2025-06-13 01:55:11 +0800

    WIP: Main Menu

---
+++ Assets/Scripts/Multiplayer/NetworkManager/MessageTypes/auth.cs
@@ -0,0 +1,14 @@
+
+public struct AuthRequest
+{
+    public string username;
+    public string password;
+}
+
+public struct AuthResponse
+{
+    public string player_id;
+    public string username;
+    public string credential_id;
+    public string password;
+}
